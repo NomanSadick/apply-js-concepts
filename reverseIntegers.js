e55e122cc306166583088f864b45052a291283f9
NomanSadick <nomansadickjs@gmail.com>
2022-10-16 22:16:05 +0600

    pairs array problem solve

--- reverseIntegers.js
+++ reverseIntegers.js
@@ -8,3 +8,21 @@ const revInt = num => {
 }
 const output = revInt(-123);
 console.log(output);
+
+// 1.
+const numbers = [1, 2, 3];
+for (var index = 0; index < numbers.length; index++);{
+  const number = numbers[index];
+  console.log(number);
+} //output undefined 
+
+// 2.
+function crazyFunction() {
+    var a = (b = 10);
+  }
+  crazyFunction();
+  console.log("b", typeof b === "undefined");//b false
+  console.log("a", typeof a === "undefined"); //a true 
+
+ 
+  
