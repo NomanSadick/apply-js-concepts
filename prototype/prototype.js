5a35e651fcf286c73513ca693aa2a9e1fea618b1
NomanSadick <nomansadickjs@gmail.com>
2022-12-19 13:04:22 +0600

    class introduction

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,24 +1,53 @@
-function Person(name, age) {
-    let person = Object.create(Person.prototype);
-    person.name = name;
-    person.age = age;
 
-    return person;
-}
 
-Person.prototype = {
+// function Person(name, age) {
+//     // let this = Object.create(PersonWithNew.prototype);
+//     this.name = name;
+//     this.age = age;
+
+//     // return this;
+// }
+
+// Person.prototype = {
+//     eat() {
+//          console.log(`Person is eating`);
+//      },
+ 
+//     sleep() {
+//          console.log(`Person is sleeping`);
+//      },
+//  }
+ 
+
+
+ 
+// const sakib = new Person('Sakib', 30);
+// sakib.eat();
+// const tamim = new Person('Tamim', 40);
+
+// class base object oriented
+
+class Person {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;    
+    }
+
     eat() {
-         console.log(`Person is eating`);
-     },
- 
-    sleep() {
-         console.log(`Person is sleeping`);
-     },
- }
- 
-const sakib = Person('Sakib', 30);
-sakib.eat()
-const tamim = Person('Tamim', 40);
+        console.log(`Person is eating`);
+    }
+
+   sleep() {
+        console.log(`Person is sleeping`);
+    }
+}
+const sakib = new Person('Sakib', 30);
+sakib.eat();
+const tamim = new Person('Tamim', 40);
+
+// example
+let person = new Array();
+console.log(Array.prototype);
 
 
 
