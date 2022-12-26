1c16072c4e4cfc5561aeb19820da60fc16a6e57c
NomanSadick <nomansadickjs@gmail.com>
2022-12-26 09:23:29 +0600

    multiple promise sametime run

--- async/promise.js
+++ async/promise.js
@@ -12,7 +12,20 @@ const meeting = new Promise((resolve, reject) => {
     }
 });
 
+// const addToCalender = (meetingDetails) => {
+//     return new Promise((resolve, reject) => {
+//         const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+//         resolve(calender)
+//     })
+// }
+
+const addToCalender = (meetingDetails) => {
+    const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+    return Promise.resolve(calender)
+}
+
 meeting
+.then(addToCalender)
 .then((res) => {
     // resolved data 
     console.log(JSON.stringify(res));
