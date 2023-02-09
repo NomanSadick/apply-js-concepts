88b2f08172234a46156cdf2354156736d8404100
NomanSadick <nomansadickjs@gmail.com>
2023-02-09 11:12:35 +0600

    updated

--- execution-context/execution-context.js
+++ execution-context/execution-context.js
@@ -13,3 +13,41 @@ function one() {
     two();
 }
 one();
+
+// scope
+function hello () {
+    const msg = "I am Johan";
+}
+
+hello();
+console.log(msg);
+
+
+// closure
+var sum = 0;
+function doSum () {
+    return function(a) {
+        return a + b;
+    }
+}
+
+var temp = doSum(2);
+sum = sum + temp(8);
+
+// global execution context is created
+let globalVar = "global"; 
+
+function firstFunction() { 
+  // function execution context for firstFunction is created
+  let firstFunctionVar = "firstFunction"; 
+
+  function secondFunction() { 
+    // function execution context for secondFunction is created
+    let secondFunctionVar = "secondFunction"; 
+    console.log(globalVar); // "global"
+    console.log(firstFunctionVar); // "firstFunction"
+    console.log(secondFunctionVar); // "secondFunction"
+  }
+  secondFunction();
+}
+firstFunction();
